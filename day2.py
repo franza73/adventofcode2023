c77e9f272cebaed170ce96bcb3f7ec767d63e4cd
Franciraldo Cavalcante <franza@Franciraldos-MacBook-Pro.local>
2023-12-02 01:56:35 -0700

    day 2 - pylint fixes

--- day2.py
+++ day2.py
@@ -10,24 +10,24 @@ def process():
     valid games.
     '''
     total = 0
-    total_power = 0
+    power = 0
     limits = {'red': 13, 'green': 14, 'blue': 15}
     with open('day2.txt', 'r', encoding="ascii") as file:
         for line in file.readlines():
             game, line = line.split(':')
             game = int(game.split()[1])
             is_valid_game = True
-            S = {'red': 0, 'green': 0, 'blue': 0}
+            score = {'red': 0, 'green': 0, 'blue': 0}
             for draw in line.split(';'):
                 for item in draw.split(','):
                     val, name = item.split()
                     if limits[name] <= int(val):
                         is_valid_game = False
-                    S[name] = max(S[name], int(val))
-            total_power += S['red'] * S['green'] * S['blue']
+                    score[name] = max(score[name], int(val))
+            power += score['red'] * score['green'] * score['blue']
             if is_valid_game:
                 total += game
-    return total, total_power
+    return total, power
 
 
 sum_of_ids, total_power = process()
